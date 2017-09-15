9b6e071b43590a961ba7cdc50fa878eaba395c2f
DanielAlejandroAnana <danana@est.frba.utn.edu.ar>
2017-09-15 11:27:47 -0300

    Add files via upload

---
+++ ProyectoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        private Induccion1Entities db = new Induccion1Entities();
+
+        // GET: /Proyecto/
+        public ActionResult Index()
+        {
+            return View(db.Proyecto.ToList());
+        }
+
+        // GET: /Proyecto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyecto.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // GET: /Proyecto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Proyecto/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ProyectoId,Nombre,FechaInicio,FechaFin")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proyecto.Add(proyecto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proyecto);
+        }
+
+        // GET: /Proyecto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyecto.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: /Proyecto/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ProyectoId,Nombre,FechaInicio,FechaFin")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proyecto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proyecto);
+        }
+
+        // GET: /Proyecto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyecto.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: /Proyecto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proyecto proyecto = db.Proyecto.Find(id);
+            db.Proyecto.Remove(proyecto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
