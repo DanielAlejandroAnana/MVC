c3ff4b223e20a68f0162b741e052f93cae7c315e
DanielAlejandroAnana <danana@est.frba.utn.edu.ar>
2017-09-26 11:57:38 -0300

    Add files via upload

---
+++ BandejaEntradaPersona.js
@@ -0,0 +1,48 @@
+﻿var txtNombre = null;
+var txtId = null;
+
+$(document).ready(function () {
+
+    txtNombre = $("[id*='txtNombre']");
+}
+);
+
+function validarBuscar() {
+
+    debugger;
+
+    cargando();
+    mensajesInvisibles();
+
+    if(txtNombre == null)
+    {
+        showError("Debe Ingresar un nombre")
+    }
+
+}
+
+function limpiarFiltros() {
+
+    txtNombre.val("");
+    return false;
+}
+
+
+function showError(mensaje) {
+    divError.show();
+    divCargando.css("display", "none");
+    lblResultadoError.text("Campos Obligatorios. " + mensaje);
+    return;
+}
+
+function mensajesInvisibles() {
+
+    divError.hide();
+    divResultado.hide();
+
+}
+
+function cargando() {
+    divCargando.css("display", "inline");
+    return true;
+}
